a865a7131bd6bc9572974ef2845c7e446c931c9c
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-06 00:45:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-      self.actors.map do |c|
-       "#{c.first_name} #{c.last_name}"
-     end.join
-    end
+     self.actors.map{|actor| actor.full_name}
+   end
 
   end
